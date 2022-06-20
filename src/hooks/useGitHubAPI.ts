88068e0d09d195defd45e4de4fb87c5dea4675bb
Jacob Heider <jacob@tea.xyz>
2022-06-20 13:23:12 -0400

    Fixes imagemagick.org

--- src/hooks/useGitHubAPI.ts
+++ src/hooks/useGitHubAPI.ts
@@ -22,11 +22,14 @@ interface GHRelease {
 export default function useGitHubAPI(): Response {
   const getVersions = async ({ user, repo, ignoredVersions }: GetVersionsOptions) => {
     const releases = await GET<GHRelease[]>(`https://api.github.com/repos/${user}/${repo}/releases`, RELOAD_POLICY)
+    // All of this will be moot once we implement proper version-mapping per package
     return releases.compactMap(({ tag_name, name }) => {
       //TODO should be explicit if you want coerce
-      const raw_version = tag_name ?? name ?? ""
+      const raw_version = (tag_name ?? name ?? "").replace(/-/, '+')
       if (ignoredVersions?.some(v => raw_version.match(v))) { return undefined }
-      return flatMap(raw_version, x => semver.coerce(x))
+
+      // This isn't great, but per-package versions.ts support will allow us to use strict parsing throughout
+      return flatMap(raw_version, x => semver.parse(x) || semver.coerce(x))
     }, { throws: true })
   }
   return { getVersions }
