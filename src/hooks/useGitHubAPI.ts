6116f954378b490de31ed9623f8ff10393194730
Jacob Heider <jacob@tea.xyz>
2022-06-20 09:24:54 -0400

    Fewer bugs if we try to trust semver.

--- src/hooks/useGitHubAPI.ts
+++ src/hooks/useGitHubAPI.ts
@@ -24,11 +24,12 @@ export default function useGitHubAPI(): Response {
     const releases = await GET<GHRelease[]>(`https://api.github.com/repos/${user}/${repo}/releases`, RELOAD_POLICY)
     // All of this will be moot once we implement proper version-mapping per package
     return releases.compactMap(({ tag_name, name }) => {
-      //TODO should be explicit if you want coerce
       const raw_version = (tag_name ?? name ?? "").replace(/-/, '+')
       if (ignoredVersions?.some(v => raw_version.match(v))) { return undefined }
 
       // This isn't great, but per-package versions.ts support will allow us to use strict parsing throughout
+      // coerce is much looser, and misses things like build IDs and
+      // pre-release identifiers
       return flatMap(raw_version, x => semver.parse(x) || semver.coerce(x))
     }, { throws: true })
   }
