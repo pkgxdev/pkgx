ec1b03250e3adc15ecf2f6feb3100c3ca42771c9
Jacob Heider <jacob@tea.xyz>
2022-07-06 16:40:22 -0400

    Try to fix test invocation

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,13 +6,16 @@ args: /bin/sh
 ---
 "
 
+## Needed for GHA
+export TMPDIR=${TMPDIR:-/tmp}
+
 deno test \
  --allow-net \
  --allow-read \
  --allow-env=SRCROOT,GITHUB_TOKEN,TMPDIR \
  --allow-run \
  --import-map=$SRCROOT/import-map.json \
- --allow-write="$TMPDIR",/tmp \
+ --allow-write="$TMPDIR" \
  tests/*.ts
 
 # /tmp is required on GHA ¯\_(ツ)_/¯
