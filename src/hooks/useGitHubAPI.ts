7a96464c4f25afe976e0bd91878c27119b5ff0da
Jacob Heider <jacob@tea.xyz>
2022-07-11 19:03:54 -0400

    Fix broken ignore regexp matching

--- src/hooks/useGitHubAPI.ts
+++ src/hooks/useGitHubAPI.ts
@@ -24,13 +24,14 @@ export default function useGitHubAPI(): Response {
     const releases = await GET<GHRelease[]>(`https://api.github.com/repos/${user}/${repo}/releases`, RELOAD_POLICY)
     // All of this will be moot once we implement proper version-mapping per package
     return releases.compactMap(({ tag_name, name }) => {
-      const raw_version = (tag_name ?? name ?? "").replace(/-/, '+')
+      const raw_version = (tag_name ?? name ?? "")
       if (ignoredVersions?.some(v => raw_version.match(v))) { return undefined }
+      const munged_raw = raw_version.replace(/-/, '+')
 
       // This isn't great, but per-package versions.ts support will allow us to use strict parsing throughout
       // coerce is much looser, and misses things like build IDs and
       // pre-release identifiers
-      return flatMap(raw_version, x => semver.parse(x) || semver.coerce(x))
+      return flatMap(munged_raw, x => semver.parse(x) || semver.coerce(x))
     }, { throws: true })
   }
   return { getVersions }
