aacd9f5261b3048ddd316f455e1fbd882e0bfcfb
Max Howell <mxcl@me.com>
2022-07-20 09:57:14 -0400

    parse then pick from both release name & tag_name

--- src/hooks/useGitHubAPI.ts
+++ src/hooks/useGitHubAPI.ts
@@ -24,14 +24,24 @@ export default function useGitHubAPI(): Response {
     const releases = await GET<GHRelease[]>(`https://api.github.com/repos/${user}/${repo}/releases`, RELOAD_POLICY)
     // All of this will be moot once we implement proper version-mapping per package
     return releases.compactMap(({ tag_name, name }) => {
-      const raw_version = (tag_name ?? name ?? "")
-      if (ignoredVersions?.some(v => raw_version.match(v))) { return undefined }
-
-      // This isn't great, but per-package versions.ts support will allow us to use strict parsing throughout
-      // coerce is much looser, and misses things like build IDs and
-      // pre-release identifiers
-      return flatMap(raw_version, x => semver.parse(x) || semver.coerce(x.replace(/-/, '+')))
+      console.debug({ tag_name, name })
+      const [v1, v2] = [v(tag_name), v(name)]
+      // v2 first as it is more likely the name of the release is well-named
+      return v2 ?? v1
     }, { throws: true })
+
+    function v(input: string | undefined) {
+      try {
+        // This isn't great, but per-package versions.ts support will allow us to use strict parsing throughout
+        // coerce is much looser, and misses things like build IDs and
+        // pre-release identifiers
+        const foundv = flatMap(input, x => semver.parse(x) || semver.coerce(x.replace(/-/, '+')))
+        if (input && foundv && ignoredVersions?.some(v => input.match(v))) { return }
+        return foundv
+      } catch {
+        //NOOP
+      }
+    }
   }
   return { getVersions }
 }
