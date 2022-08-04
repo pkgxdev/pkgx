892dc03a09d79c031e00b9374e7038dc502549c2
Max Howell <mxcl@me.com>
2022-08-04 11:34:34 -0400

    Use GraphQL for tags (requires auth :-/)

--- src/hooks/useGitHubAPI.ts
+++ src/hooks/useGitHubAPI.ts
@@ -1,5 +1,5 @@
 import { RELOAD_POLICY } from "mxcl/deno-cache";
-import { GET, isArray } from "utils"
+import { GET, isArray, undent, validateArray, validateString } from "utils"
 
 //TODO pagination
 
@@ -19,15 +19,56 @@ interface GHRelease {
 }
 
 export default function useGitHubAPI(): Response {
-  const getVersions = async ({ user, repo, type }: GetVersionsOptions) => {
-    const endpoint = type.startsWith("releases") ? 'releases' : 'tags'
-    const json = await GET<GHRelease[]>(`https://api.github.com/repos/${user}/${repo}/${endpoint}?per_page=100`, RELOAD_POLICY)
-    if (!isArray(json)) throw "unexpected json"
-    return json.map(({ tag_name, name }) => type == 'releases/tags'
-      ? tag_name
-      : name
-    )
-  }
-
   return { getVersions }
+}
+
+async function getVersions({ user, repo, type }: GetVersionsOptions): Promise<string[]> {
+  //TODO set `Accept: application/vnd.github+json`
+  //TODO we can use ETags to check if the data we have cached is still valid
+
+  if (type.startsWith("releases")) {
+    const json = await GET<GHRelease[]>(`https://api.github.com/repos/${user}/${repo}/releases?per_page=100`, RELOAD_POLICY)
+    if (!isArray(json)) throw "unexpected json"
+    return json.map(({ tag_name, name }) => type == 'releases/tags' ? tag_name : name)
+  } else {
+    // github tags API returns in reverse alphabetical order lol
+    // so we have to use their graphql endpoint
+    // sadly the graph ql endpoint requires auth :/
+
+    //NOTE realistically the bad sort order for the REST api only effects ~5% of projects
+    // so potentially could flag those projects (eg. go.dev)
+
+    const headers: HeadersInit = {}
+    const token = Deno.env.get("GITHUB_TOKEN")
+    if (token) headers['Authorization'] = `bearer ${token}`
+
+    const query = undent`
+      query {
+        repository(owner: "${user}", name: "${repo}") {
+          refs(last: 100, refPrefix: "refs/tags/", orderBy: {field: TAG_COMMIT_DATE, direction: ASC}) {
+            nodes {
+              name
+            }
+          }
+        }
+      }`
+    const rsp = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify({ query }),
+      headers
+    })
+    const json = await rsp.json()
+
+    if (!rsp.ok) {
+      console.error({ rsp, json })
+      throw new Error()
+    } else {
+      console.debug(json)
+    }
+
+    // deno-lint-ignore no-explicit-any
+    const foo = validateArray(json?.data?.repository?.refs?.nodes).map((x: any) => validateString(x?.name))
+
+    return foo
+  }
 }
