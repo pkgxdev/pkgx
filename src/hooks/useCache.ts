abf94266834925770c516c04ce75c5e54984f575
Max Howell <mxcl@me.com>
2022-09-02 11:22:45 -0400

    Tidy useCache

--- src/hooks/useCache.ts
+++ src/hooks/useCache.ts
@@ -1,5 +1,5 @@
 import { Package, Path, semver } from "types"
-import { download, packageSort } from "utils"
+import * as utils from "utils"
 import usePlatform from "hooks/usePlatform.ts"
 import * as _ from "utils" // console.verbose
 
@@ -9,73 +9,67 @@ interface DownloadOptions {
   type?: 'src' | 'bottle'
 }
 
-interface Response {
-  /// destination for bottle downloads
-  bottle(pkg: Package): Path
-  /// key used when downloading from our S3 bottle storage
-  s3Key(pkg: Package): string
-  /// download source or bottle
-  download(opts: DownloadOptions): Promise<Path>
-  /// lists all packages with bottles in the cache
-  ls(): Promise<Package[]>
+const prefix = new Path("/opt/tea.xyz/var/www")
+
+export default function useCache() {
+  return { download, bottle, ls, s3Key }
 }
 
-export default function useCache(): Response {
-  const prefix = new Path("/opt/tea.xyz/var/www")
-  const stem = (pkg: Package) => {
-    const name = pkg.project.replaceAll("/", "∕")
-    // ^^ OHAI, we’re replacing folder slashes with unicode slashes
-    return `${name}-${pkg.version}`
-  }
+const stem = (pkg: Package) => {
+  const name = pkg.project.replaceAll("/", "∕")
+  // ^^ OHAI, we’re replacing folder slashes with unicode slashes
+  return `${name}-${pkg.version}`
+}
 
-  const bottle = (pkg: Package) => {
-    const { arch, platform } = usePlatform()
-    return prefix.join(`${stem(pkg)}+${platform}+${arch}.tar.gz`)
-  }
+/// destination for bottle downloads
+const bottle = (pkg: Package) => {
+  const { arch, platform } = usePlatform()
+  return prefix.join(`${stem(pkg)}+${platform}+${arch}.tar.gz`)
+}
 
-  const download = async ({ url: readURL, pkg, type = 'bottle' }: DownloadOptions) => {
-    const filename = (() => {
-      switch(type!) {
-        case 'src': return stem(pkg) + Path.extname(readURL)
-        case 'bottle': return bottle(pkg).string
-      }
-    })()
-    const writeFilename = prefix.join(filename)
-    console.debug(writeFilename)
-    if (writeFilename.isReadableFile()) {
-      console.info({alreadyDownloaded: writeFilename})
-    } else {
-      console.info({downloading: readURL})
-      await grab({ readURL, writeFilename })
+/// download source or bottle
+const download = async ({ url: readURL, pkg, type = 'bottle' }: DownloadOptions) => {
+  const filename = (() => {
+    switch(type!) {
+      case 'src': return stem(pkg) + Path.extname(readURL)
+      case 'bottle': return bottle(pkg).string
     }
-    return writeFilename
+  })()
+  const writeFilename = prefix.join(filename)
+  console.debug(writeFilename)
+  if (writeFilename.isReadableFile()) {
+    console.info({alreadyDownloaded: writeFilename})
+  } else {
+    console.info({downloading: readURL})
+    await grab({ readURL, writeFilename })
+  }
+  return writeFilename
+}
+
+/// lists all packages with bottles in the cache
+const ls = async () => {
+  const { arch, platform } = usePlatform()
+
+  const rv = []
+
+  for await (const file of prefix.ls()) {
+    const match = file[1].name.match(`^(.*)-([0-9]+\\.[0-9]+\\.[0-9]+)\\+${platform}\\+${arch}\\.tar\\.gz$`)
+    if (!match) { continue }
+    const [_, p, v] = match
+    // Gotta undo the package name manipulation to get the package from the bottle
+    const project = p.replaceAll("∕", "/")
+    const version = semver.coerce(v)
+    if (!version) { continue }
+    rv.push({ project, version })
   }
 
-  const ls = async () => {
-    const { arch, platform } = usePlatform()
+  return rv.sort(utils.packageSort)
+}
 
-    const rv = []
-
-    for await (const file of prefix.ls()) {
-      const match = file[1].name.match(`^(.*)-([0-9]+\\.[0-9]+\\.[0-9]+)\\+${platform}\\+${arch}\\.tar\\.gz$`)
-      if (!match) { continue }
-      const [_, p, v] = match
-      // Gotta undo the package name manipulation to get the package from the bottle
-      const project = p.replaceAll("∕", "/")
-      const version = semver.coerce(v)
-      if (!version) { continue }
-      rv.push({ project, version })
-    }
-
-    return rv.sort(packageSort)
-  }
-
-  const s3Key = (pkg: Package) => {
-    const { platform, arch } = usePlatform()
-    return `${pkg.project}/${platform}/${arch}/v${pkg.version.version}.tar.gz`
-  }
-
-  return { download, bottle, ls, s3Key }
+/// key used when downloading from our S3 bottle storage
+const s3Key = (pkg: Package) => {
+  const { platform, arch } = usePlatform()
+  return `${pkg.project}/${platform}/${arch}/v${pkg.version.version}.tar.gz`
 }
 
 async function grab({ readURL, writeFilename }: { readURL: string, writeFilename: Path }) {
@@ -86,6 +80,6 @@ async function grab({ readURL, writeFilename }: { readURL: string, writeFilename
   verbose({downloading: readURL})
   verbose({destination: writeFilename})
 
-  const file = await download(readURL)
+  const file = await utils.download(readURL)
   await Deno.link(file.path, writeFilename.string)
 }
