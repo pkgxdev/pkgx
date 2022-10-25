4b6d11402107af31da257cba0f49e0dde52470d8
Max Howell <mxcl@me.com>
2022-10-25 14:24:06 -0400

    test our sync-lock doesn’t hang (#96)

--- src/hooks/usePantry.git.ts
+++ src/hooks/usePantry.git.ts
@@ -1,56 +1,64 @@
-import { install as tea_install, hydrate, resolve } from "prefab"
-import { flatten } from "./useShellEnv.ts"
-import { useDownload, useShellEnv, usePrefix } from "hooks"
+import { useDownload, usePrefix, useCellar } from "hooks"
 import * as semver from "semver"
 import { run } from "utils"
 import Path from "path"
 
 export const prefix = usePrefix().join('tea.xyz/var/pantry/projects')
 
-async function find_git(): Promise<[Path | string, Record<string, string[]>] | undefined> {
+async function find_git(): Promise<Path | undefined> {
   for (const path_ of Deno.env.get('PATH')?.split(':') ?? []) {
     const path = Path.root.join(path_, 'git')
     if (path.isExecutableFile()) {
-      return [path, {}]
+      return Promise.resolve(path)
     }
   }
 
-  try {
-    const installations = await (async () => {
-      const { pkgs: wet } = await hydrate({ project: 'git-scm.org', constraint: new semver.Range('*') })
-      const { pending: gas, installed } = await resolve(wet)
-      return [
-        ...await Promise.all(gas.map(tea_install)),
-        ...installed
-      ]
-    })()
-    const env = useShellEnv({ installations })
-    return ['git', env]
-  } catch (err) {
-    console.warn(err)
-  }
+  const pkg = {project: 'git-scm.org', constraint: new semver.Range('*')}
+  const git = await useCellar().has(pkg)
+  return git?.path.join('bin/git')
+
+  //ALERT! don’t install git with tea
+  // there's no pantry yet, so attempting to do so will infinitely recurse
 }
 
 const pantry_dir = prefix.parent()
 const pantries_dir = pantry_dir.parent().join("pantries")
 
+let avoid_softlock = false
+
+async function lock<T>(body: () => Promise<T>) {
+  if (avoid_softlock) throw new Error("aborting to prevent softlock")
+  avoid_softlock = true
+
+  const { rid } = Deno.openSync(pantry_dir.mkpath().string)
+  await Deno.flock(rid, true)
+
+  try {
+    return await body()
+  } finally {
+    //TODO if this gets stuck then nothing will work so need a handler for that
+    await Deno.funlock(rid)
+    avoid_softlock = false
+  }
+}
+
 //TODO we have a better system in mind than git
 export async function install(): Promise<true | 'not-git' | 'noop' | 'deprecated'> {
-  if (pantries_dir.exists()) return 'noop'
   if (prefix.exists()) {
-    return pantry_dir.join('.git').exists()
-      ? 'deprecated' // tmp to do internal fix
-      : 'not-git'
+    if (pantries_dir.exists()) return 'noop'
+    if (pantry_dir.join('.git').exists()) return 'deprecated'
+
+    // FIXME in this case we have a downloaded pantry so we should install git
+    return 'not-git'
   }
 
   try {
-    const { rid } = Deno.openSync(pantry_dir.mkpath().string)
-    await Deno.flock(rid, true)
-    try {
+    const git = await find_git()
+
+    return await lock(async () => {
       if (prefix.exists()) return 'noop'
       // ^^ another instance of tea did the install while we waited
 
-      const git = await find_git()
       if (git) {
         await clone(git)
         return true
@@ -58,19 +66,14 @@ export async function install(): Promise<true | 'not-git' | 'noop' | 'deprecated
         await unzip()
         return 'not-git'
       }
-    } finally {
-      //TODO if this gets stuck then nothing will work so need a handler for that
-      await Deno.funlock(rid)
-    }
+    })
   } catch (e) {
     pantries_dir.rm({ recursive: true }) // leave us in a blank state
     pantry_dir.rm({ recursive: true })   // ^^
     throw e
   }
 
-  async function clone([git, preenv]: [Path | string, Record<string, string[]>]) {
-    const env = flatten(preenv)
-
+  async function clone(git: Path) {
     const pp: Promise<void>[] = []
     for (const name of ["pantry.core", "pantry.extra"]) {
       const p = run({
@@ -79,14 +82,13 @@ export async function install(): Promise<true | 'not-git' | 'noop' | 'deprecated
             "--bare", "--depth=1",
             `https://github.com/teaxyz/${name}`,
             pantries_dir.join("teaxyz").mkpath().join(name)
-        ],
-        env
+        ]
       })
       pp.push(p)
     }
 
     await Promise.all(pp)
-    await co(git, env)
+    await co(git)
   }
 
   async function unzip() {
@@ -113,7 +115,7 @@ async function *ls() {
 export const update = async () => {
   switch (await install()) {
   case 'deprecated':
-    console.warn("pantry is a clone, this is deprecated, cannot update, please reinstall")
+    console.warn("pantry is a clone, this is deprecated, please clean-install tea")
     break
   case 'not-git':
     console.warn("pantry is not a git repository, cannot update")
@@ -121,14 +123,13 @@ export const update = async () => {
   case 'noop': {
     const git = await find_git()
     if (!git) return console.warn("cannot update pantry without git")
-    const env = flatten(git[1])
     const pp: Promise<void>[] = []
     for await (const cwd of ls()) {
-      const p = run({cmd: [git[0], "fetch", "origin"], cwd, env })
+      const p = run({cmd: [git, "fetch", "origin"], cwd })
       pp.push(p)
     }
     await Promise.all(pp)
-    await co(git[0], env)
+    await co(git)
   } break
   default:
     break // we don’t update if we only just cloned it
@@ -137,7 +138,7 @@ export const update = async () => {
 
 //FIXME order matters
 //NOTE well this overlay method is not permanent for sure
-async function co(git: string | Path, env: Record<string, string>) {
+async function co(git: string | Path) {
   for await (const git_dir of ls()) {
     const cmd = [git,
       "--git-dir", git_dir,
@@ -145,6 +146,6 @@ async function co(git: string | Path, env: Record<string, string>) {
       "checkout",
       "--force"
     ]
-    await run({ cmd, env })
+    await run({ cmd })
   }
 }
