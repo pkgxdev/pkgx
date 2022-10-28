3026e450b549cb51d11c662ff61b487de3c04ce5
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2022-10-28 18:22:19 +0530

    fixed type error

--- src/hooks/useBinaryRepository.ts
+++ src/hooks/useBinaryRepository.ts
@@ -3,10 +3,10 @@ import { Stowage } from 'types';
 
 export default async function useBinaryRepository(stowage: Stowage){
     const ipfsUrl = await useOffLicense('ipfs').url(stowage)
-    
+
     if(ipfsUrl == "No CID file in S3"){
-        return useOffLicense('s3').url(stowage)
+        return new URL(await useOffLicense('s3').url(stowage))
     }
-    else return ipfsUrl
+    else return new URL(ipfsUrl)
     
 }
