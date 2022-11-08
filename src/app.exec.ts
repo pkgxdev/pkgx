780c7b985b963a34c44c70a18c5f7b62f975b042
David Tai <dtaipublic@gmail.com>
2022-11-08 08:22:20 -0500

    Basic Path Sanitization (#131)
    Prevent `tea .` from trying to overwrite the directory with its temporary shell file by adding some sanitization.  Also prevent people from trying to escape the tmp directory.
    
    Co-authored-by: Max Howell <mxcl@me.com>

--- src/app.exec.ts
+++ src/app.exec.ts
@@ -245,8 +245,11 @@ async function abracadabra(opts: Args): Promise<RV> {
 
     //FIXME putting "$@" at the end can be invalid, it really depends on the script TBH
     //FIXME shouldn’t necessarily default to bash
+    
+    // This is short term until a longer term fix is available through a deno library 
+    const saferArg0 = arg0 === '.' ? 'sh' : arg0.replaceAll('../', '')
 
-    const path = Path.mktmp().join(arg0).write({ force: true, text: undent`
+    const path = Path.mktmp().join(saferArg0).write({ force: true, text: undent`
       #!/bin/bash
       set -e
       ${debug ? "set -x" : ""}
