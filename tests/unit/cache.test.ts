79d1a4dde8a2ea58e55bd92965aa2791ca0e7c91
kunalgoyal9 <kunalgoyal.goyal9@gmail.com>
2022-11-11 18:58:44 +0530

    added test to check for mtime and etag files

---
+++ tests/unit/cache.test.ts
@@ -0,0 +1,24 @@
+import { assert } from "deno/testing/asserts.ts"
+import { useDownload } from "hooks"
+import { sandbox } from "../utils.ts"
+
+Deno.test("etag-mtime-check",async () => {
+  await sandbox(async tmpdir => {
+    const src = new URL("https://dist.tea.xyz/deno.land/darwin/aarch64/v1.27.1.tar.xz")
+    const dst = await useDownload().download({src, dst: tmpdir.join("test.tar.xz")})
+
+    const mtimePath = await useDownload().hash_key(src).join("mtime")
+    const etagPath = await useDownload().hash_key(src).join("etag")
+
+    const mtime = await mtimePath.read()
+    const etag = await etagPath.read()
+    
+    const rsp = await fetch(src, {})
+    const mtimeA = rsp.headers.get("Last-Modified")
+    const etagA = rsp.headers.get("etag")
+
+    assert(mtimeA === mtime)
+    assert(etagA === etag)
+    await rsp.body?.cancel()
+  })
+})
