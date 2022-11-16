7955d49ffe1a22a10d85daf3b10ad84d83311b89
Max Howell <mxcl@me.com>
2022-11-16 11:53:14 -0500

    More careful code for all of `-x` mode

--- src/app.sync.ts
+++ src/app.sync.ts
@@ -1,7 +1,8 @@
 import useFlags, { Args } from "hooks/useFlags.ts"
 import { install, link, resolve } from "prefab"
 import { useSync, useVirtualEnv } from "hooks"
-import { Logger } from "./hooks/useLogger.ts"
+
+//TODO app.exec.ts and app.dump.ts should handle updating packages as part of their install logics
 
 export default async function sync(opts: Args) {
   const { magic } = useFlags()
@@ -12,19 +13,18 @@ export default async function sync(opts: Args) {
   const pkgs = opts.pkgs
 
   if (opts.env) {
-    pkgs.push(...(await useVirtualEnv()).requirements)
+    pkgs.push(...(await useVirtualEnv()).pkgs)
   } else if (magic) {
     // TODO shouldn’t use magic if user has explicitly passed eg. a script path
     const blueprint = await useVirtualEnv().swallow(/^not-found/)
     if (blueprint) {
-      pkgs.push(...blueprint.requirements)
+      pkgs.push(...blueprint.pkgs)
     }
   }
 
   if (pkgs.length) {
     for (const pkg of (await resolve(pkgs, { update: true })).pending) {
-      const logger = new Logger()
-      const installation = await install(pkg, logger)
+      const installation = await install(pkg)
       await link(installation)
     }
   }
