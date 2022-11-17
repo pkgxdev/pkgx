9e91a2e4f6f922e26f781ef9c5ab6bc4a543772d
Max Howell <mxcl@me.com>
2022-11-17 10:34:32 -0500

    Fix lint

--- src/hooks/usePantry.ts
+++ src/hooks/usePantry.ts
@@ -181,7 +181,7 @@ async function entry({ project }: { project: string }): Promise<Entry> {
     const versions = dir.join("versions.txt")
     return { dir, yml, versions }
   }
-  
+
   const closestPkg = await getClosestPackageSuggestion(project);
   throw new TeaError('not-found: pantry: package.yml', {project, closestPkg}, )
 }
@@ -201,18 +201,19 @@ async function getClosestPackageSuggestion(orgPkg:string): Promise<string>{
   })
   return closestPkg;
 }
+
 function levenshteinDistance (str1: string, str2:string):number{
   const track = Array(str2.length + 1).fill(null).map(() =>
-  Array(str1.length + 1).fill(null));
+    Array(str1.length + 1).fill(null))
   for (let i = 0; i <= str1.length; i += 1) {
-     track[0][i] = i;
+     track[0][i] = i
   }
   for (let j = 0; j <= str2.length; j += 1) {
-     track[j][0] = j;
+     track[j][0] = j
   }
   for (let j = 1; j <= str2.length; j += 1) {
      for (let i = 1; i <= str1.length; i += 1) {
-        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1
         track[j][i] = Math.min(
            track[j][i - 1] + 1, // deletion
            track[j - 1][i] + 1, // insertion
@@ -220,8 +221,9 @@ function levenshteinDistance (str1: string, str2:string):number{
         );
      }
   }
-  return track[str2.length][str1.length];
-};
+  return track[str2.length][str1.length]
+}
+
 /// returns sorted versions
 async function getVersions(spec: Package | PackageRequirement): Promise<SemVer[]> {
   const files = await entry(spec)
