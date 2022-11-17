02886d31a4e1e66699a4e05a841cd8860c3989ec
chirag.gandhi <chirag.gandhi>
2022-11-17 11:48:00 +0530

    added fuzzy string check logic using levenshteinDistance algorithm

--- src/hooks/usePantry.ts
+++ src/hooks/usePantry.ts
@@ -181,9 +181,52 @@ function entry({ project }: { project: string }): Entry {
     const versions = dir.join("versions.txt")
     return { dir, yml, versions }
   }
-  throw new TeaError('not-found: pantry: package.yml', {project})
-}
+  //check for fuzzy string
+  const closestPkg = getClosestPackageSuggestion(project, prefix);
+  closestPkg.then(function(data){
+    console.log("did you mean", {data}, "?")
+  });
+  throw new TeaError('not-found: pantry: package.yml', {project}, )
 
+}
+async function getClosestPackageSuggestion(orgPkg:string, projectPath:Path): Promise<string>{
+  let closestPkg = '';
+  let minDistance = Infinity;
+  const pkgList = [];
+  for await (const data of projectPath.ls()){
+    const {name} = data[1];
+    pkgList.push(name);
+  }
+  pkgList.forEach(pkgName=>{
+    const number = levenshteinDistance(pkgName, orgPkg);
+    if(number<minDistance){
+      minDistance = number;
+      closestPkg = pkgName;
+    }
+  })
+  return closestPkg;
+}
+function levenshteinDistance (str1: string, str2:string):number{
+  const track = Array(str2.length + 1).fill(null).map(() =>
+  Array(str1.length + 1).fill(null));
+  for (let i = 0; i <= str1.length; i += 1) {
+     track[0][i] = i;
+  }
+  for (let j = 0; j <= str2.length; j += 1) {
+     track[j][0] = j;
+  }
+  for (let j = 1; j <= str2.length; j += 1) {
+     for (let i = 1; i <= str1.length; i += 1) {
+        const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+        track[j][i] = Math.min(
+           track[j][i - 1] + 1, // deletion
+           track[j - 1][i] + 1, // insertion
+           track[j - 1][i - 1] + indicator, // substitution
+        );
+     }
+  }
+  return track[str2.length][str1.length];
+};
 /// returns sorted versions
 async function getVersions(spec: Package | PackageRequirement): Promise<SemVer[]> {
   const files = entry(spec)
