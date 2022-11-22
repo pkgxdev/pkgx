84168336d9cfc5d1a3e0bdf7675b730905959859
Max Howell <mxcl@me.com>
2022-11-22 10:22:32 -0500

    fix saying pantry not found when it’s fine

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ All you need is `tea`.
 &nbsp;
 
 
-# tea/cli 0.13.9
+# tea/cli 0.13.10
 
 Open source is a treasure trove—yet those chests are sealed with gnarly locks.
 tea is the key:
--- src/hooks/usePantry.ts
+++ src/hooks/usePantry.ts
@@ -167,8 +167,8 @@ function coerceNumber(input: any) {
 
 function entry({ project }: { project: string }): Entry {
   for (const prefix of pantry_paths()) {
+    if (!prefix.exists()) throw new TeaError('not-found: pantry', { path: prefix })
     const dir = prefix.join(project)
-    if (!dir.exists()) throw new TeaError('not-found: pantry', { path: prefix })
     const filename = dir.join("package.yml")
     if (!filename.exists()) continue
     const yml = async () => {
