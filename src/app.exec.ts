94f2c2051958a25e8781ead96179861692086e8a
Max Howell <mxcl@me.com>
2023-01-17 07:27:11 -0500

    Better errors for permission denied
    Refs: https://github.com/teaxyz/cli/issues/313

--- src/app.exec.ts
+++ src/app.exec.ts
@@ -4,6 +4,7 @@ import { useFlags } from "hooks"
 import { gray, red, teal } from "hooks/useLogger.ts"
 import { basename } from "deno/path/mod.ts"
 import { isNumber } from "is_what"
+import Path from "path"
 
 export default async function(cmd: string[], env: Record<string, string>) {
 
@@ -16,16 +17,20 @@ export default async function(cmd: string[], env: Record<string, string>) {
     await run({cmd, env})
   } catch (err) {
     const { debug } = useFlags()
+    const arg0 = cmd?.[0]
 
     if (err instanceof TeaError) {
       throw err
     } else if (debug) {
       console.error(err)
     } else if (err instanceof Deno.errors.NotFound) {
-      // deno-lint-ignore no-explicit-any
-      const { cmd } = (err as any)
-      console.error("tea: command not found:", teal(cmd?.[0]))
-      //NOTE ^^ we add cmd into the error ourselves in utils/ru
+      console.error("tea: command not found:", teal(arg0))
+    } else if (err instanceof Deno.errors.PermissionDenied) {
+      if (Path.abs(arg0)?.isDirectory()) {
+        console.error("tea: is directory:", teal(arg0))
+      } else {
+        console.error("tea: permission denied:", teal(arg0))
+      }
     } else if (err instanceof RunError == false) {
       const decapitalize = ([first, ...rest]: string) => first.toLowerCase() + rest.join("")
       console.error(`${red("error")}:`, decapitalize(err.message))
