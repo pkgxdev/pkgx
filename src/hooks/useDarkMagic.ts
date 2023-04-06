e62c6c2b5612b95841da9abb3734850729e5efb3
Jacob Heider <jacob@tea.xyz>
2023-04-06 16:00:04 -0400

    +pipx

--- src/hooks/useDarkMagic.ts
+++ src/hooks/useDarkMagic.ts
@@ -9,6 +9,7 @@ const which = async (arg0: string | undefined) => {
   // Query external providers; return commands to run if found
   const found = await Promise.all([
     npx(arg0),
+    pipx(arg0),
   ])
 
   // Return the first non-empty result
@@ -23,6 +24,20 @@ const npx = async (arg0: string | undefined) => {
       project: "npmjs.com",
       constraint: new Range("*"),
       shebang: ["npx", "-y"]
+    }
+  }
+
+  return undefined
+}
+
+const pipx = async (arg0: string | undefined) => {
+  const res = await useFetch(`https://pypi.org/pypi/${arg0}/json`)
+
+  if (res.status == 200) {
+    return {
+      project: "pypa.github.io/pipx",
+      constraint: new Range("*"),
+      shebang: ["pipx", "run"]
     }
   }
 
