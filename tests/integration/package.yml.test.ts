38c517c95ac0f155efa6771aa13c4032a4edab80
Max Howell <mxcl@me.com>
2023-04-07 07:35:36 -0400

    test runtime.env moustaches

---
+++ tests/integration/package.yml.test.ts
@@ -0,0 +1,33 @@
+import suite from "../integration.suite.ts"
+import { it } from "deno/testing/bdd.ts"
+import { assertEquals } from "deno/testing/asserts.ts"
+import { undent } from "utils"
+import Path from "path"
+
+it(suite, "runtime.env tildes", async function() {
+  const run = async (FOO: string) => {
+    this.sandbox.join("projects/tea.xyz/foo").mkpath().join("package.yml").write({ text: undent`
+      provides: [bin/foo]
+      runtime:
+        env:
+          FOO: "${FOO}"
+      `, force: true})
+
+    this.sandbox.join("tea.xyz/foo/v1.0.0/bin").mkpath().join("foo").write({ text: undent`
+      #!/bin/sh
+      echo "$FOO"
+      `, force: true}).chmod(0o755)
+
+    const out = await this.run({
+      args: ["foo"],
+      env: {
+        TEA_PANTRY_PATH: this.sandbox.string,
+        TEA_PREFIX: this.sandbox.string
+      }
+    }).stdout()
+
+    assertEquals(out.trim(), FOO.replaceAll("{{home}}", Path.home().string))
+  }
+
+  await run("{{home}}/foo")
+})
