2f59bfa5e661a8f2f62e4d11cdcbed7150a537af
Max Howell <mxcl@me.com>
2023-05-23 13:54:32 -0400

    wip

--- src/hooks/useExec.install.ts
+++ src/hooks/useExec.install.ts
@@ -1,14 +1,14 @@
 import { PackageSpecification, Package, utils, Installation, prefab } from "tea"
 const { hydrate, link, resolve, install } = prefab
+import useLogger, { Logger } from "./useLogger.ts"
 import { ExitError } from "./useErrorHandler.ts"
 import useConfig from "./useConfig.ts"
-import useLogger from "./useLogger.ts"
 import undent from "outdent"
 
 export default async function(pkgs: PackageSpecification[], update: boolean) {
-  const { modifiers: { json, dryrun, verbosity }, env, prefix } = useConfig()
+  const { modifiers: { json, dryrun }, env, prefix } = useConfig()
   const logger = useLogger().new()
-  const { logJSON, gray, teal } = useLogger()
+  const { logJSON, gray } = useLogger()
 
   if (!json) {
     logger.replace("resolving package graph")
@@ -16,20 +16,14 @@ export default async function(pkgs: PackageSpecification[], update: boolean) {
     logJSON({ status: "resolving" })
   }
 
-  const pkg_prefix_str = (pkg: Package) => [
-    gray(prefix.prettyString()),
-    pkg.project,
-    `${gray('v')}${pkg.version}`
-  ].join(gray('/'))
-
-  console.debug({hydrating: pkgs})
-
   const { pkgs: wet, dry } = await hydrate(pkgs)
   const {installed, pending} = await resolve(wet, { update })
-  logger.clear()
 
   if (json) {
     logJSON({ status: "resolved", pkgs: pending.map(utils.pkg.str) })
+  } else {
+    logger.clear()
+    console.debug({hydrating: pkgs})
   }
 
   if (!dryrun && env.TEA_MAGIC?.split(':').includes("prompt")) {
@@ -53,71 +47,14 @@ export default async function(pkgs: PackageSpecification[], update: boolean) {
   }
 
   for (const pkg of pending) {
-    const log_install_msg = (pkg: Package, title = 'installed') => {
-      if (json) {
-        logJSON({status: title, pkg: utils.pkg.str(pkg)})
-      } else {
-        const str = pkg_prefix_str(pkg)
-        logger!.replace(`${title}: ${str}`, { prefix: false })
-      }
-    }
-
     let installation: Installation
+    const logger = useLogger().new(gray(utils.pkg.str(pkg)))
 
     if (dryrun) {
       installation = { pkg, path: prefix.join(pkg.project, `v${pkg.version}`) }
-      log_install_msg(pkg, 'imagined')
+      log_installed_msg(pkg, 'imagined', logger)
     } else {
-      let bytes = 0
-      const timestamp = Date.now()
-      installation = await install(pkg, {
-        locking: () => {
-          if (!json) {
-            logger.replace(teal("locking"))
-          } else {
-            logJSON({status: "locking", pkg: utils.pkg.str(pkg) })
-          }
-        },
-        /// raw http info
-        downloading: ({pkg, src, dst, rcvd, total}) => {
-          if (json) {
-            logJSON({status: "downloading", "received": rcvd, "content-size": total, pkg, src, dst })
-          } else if (verbosity >= 0) {
-            bytes = rcvd ?? 0
-          } else if (total) {
-            logger.replace(`installing: ${pkg_prefix_str(pkg)} (${pretty_size(total)})`)
-          } else {
-            logger.replace(`installing: ${pkg_prefix_str(pkg)}`)
-          }
-        },
-        installing: ({pkg, progress}) => {
-          if (json) {
-            logJSON({status: "installing", pkg, progress })
-          } else if (verbosity >= 0) {
-            let s = teal("installing")
-            let pc = (progress ?? 0) * 100;
-            pc = pc < 1 ? Math.round(pc) : Math.floor(pc);  // don’t say 100% at 99.5%
-
-            s += ` ${pc}%`.padEnd(4, ' ')
-
-            const duration = Date.now() - timestamp
-            if (duration > 0) {
-              const speed = bytes / duration * 1000
-              let dl = pretty_size(speed)
-              dl += "/s"
-              s += ` ${gray(dl)}`
-            }
-
-            logger.replace(s)
-          }
-        },
-        unlocking: (pkg: Package) => {
-          if (json) logJSON({status: "unlocking", pkg: utils.pkg.str(pkg) })
-        },
-        installed: (installation: Installation) => {
-          log_install_msg(installation.pkg)
-        }
-      })
+      installation = await i(pkg, logger)
       await link(installation)
     }
 
@@ -126,6 +63,76 @@ export default async function(pkgs: PackageSpecification[], update: boolean) {
 
   return { installed, dry }
 }
+
+async function i(pkg: Package, logger: Logger) {
+  const { modifiers: { json, verbosity } } = useConfig()
+  const { logJSON, teal, gray } = useLogger()
+
+  let bytes = 0
+  let content_size: number | undefined
+  const timestamp = Date.now()
+
+  return await install(pkg, {
+    locking: () => {
+      if (!json) {
+        logger.replace(teal("locking"))
+      } else {
+        logJSON({status: "locking", pkg: utils.pkg.str(pkg) })
+      }
+    },
+    /// raw http info
+    downloading: ({pkg, src, dst, rcvd, total}) => {
+      if (json) {
+        logJSON({status: "downloading", "received": rcvd, "content-size": total, pkg, src, dst })
+      } else if (verbosity >= 0) {
+        bytes = rcvd ?? 0
+        content_size = total
+      } else if (total) {
+        content_size = total
+        logger.replace(`${teal('installing')} ${gray(pretty_size(total))}`)
+      } else {
+        logger.replace(`${teal('installing')}`)
+      }
+    },
+    installing: ({pkg, progress}) => {
+      if (json) {
+        logJSON({status: "installing", pkg, progress })
+      } else if (verbosity >= 0) {
+        let s = teal("installing")
+
+        let pc = (progress ?? 0) * 100;
+        pc = pc < 1 ? Math.round(pc) : Math.floor(pc);  // don’t say 100% at 99.5%
+
+        s += ` ${pc}%`.padEnd(4, ' ')
+
+        const duration = Date.now() - timestamp
+        if (duration > 0) {
+          const speed = bytes / duration * 1000
+          let dl = pretty_size(speed)
+          dl += "/s"
+          s += ` ${gray(dl)}`
+        }
+
+        if (content_size) {
+          const a = pretty_size(bytes)
+          const b = pretty_size(content_size)
+          s += ` ${gray(`${a}/${b}`)}`
+        }
+
+        logger.replace(s)
+      }
+    },
+    unlocking: (pkg: Package) => {
+      if (json) logJSON({status: "unlocking", pkg: utils.pkg.str(pkg) })
+    },
+    installed: (installation: Installation) => {
+      log_installed_msg(installation.pkg, 'installed', logger)
+    }
+  })
+}
+
+
+////////////////// utils //////////////////
 
 function pretty_size(n: number) {
   const units = ["B", "KiB", "MiB", "GiB", "TiB"]
@@ -136,4 +143,22 @@ function pretty_size(n: number) {
   }
   const precision = n < 10 ? 2 : n < 100 ? 1 : 0
   return `${n.toFixed(precision)} ${units[i]}`
+}
+
+const log_installed_msg = (pkg: Package, title: string, logger: Logger) => {
+  const { prefix, modifiers: { json } } = useConfig()
+  const { gray, logJSON } = useLogger()
+
+  const pkg_prefix_str = (pkg: Package) => [
+    gray(prefix.prettyString()),
+    pkg.project,
+    `${gray('v')}${pkg.version}`
+  ].join(gray('/'))
+
+  if (json) {
+    logJSON({status: title, pkg: utils.pkg.str(pkg)})
+  } else {
+    const str = pkg_prefix_str(pkg)
+    logger!.replace(`${title}: ${str}`, { prefix: false })
+  }
 }
