c6b9e45c26d466bc796846dc592332ee7633b901
Max Howell <mxcl@me.com>
2023-05-25 10:42:30 -0400

    Update to libtea 0.1.5

--- tests/integration/package.yml.test.ts
+++ tests/integration/package.yml.test.ts
@@ -6,14 +6,14 @@ import { Path } from "tea"
 
 it(suite, "runtime.env tildes", async function() {
   const run = async (FOO: string) => {
-    this.sandbox.join("projects/tea.xyz/foo").mkpath().join("package.yml").write({ text: undent`
+    this.sandbox.join("projects/tea.xyz/foo").mkdir('p').join("package.yml").write({ text: undent`
       provides: [bin/foo]
       runtime:
         env:
           FOO: "${FOO}"
       `, force: true})
 
-    this.sandbox.join("tea.xyz/foo/v1.0.0/bin").mkpath().join("foo").write({ text: undent`
+    this.sandbox.join("tea.xyz/foo/v1.0.0/bin").mkdir('p').join("foo").write({ text: undent`
       #!/bin/sh
       echo "$FOO"
       `, force: true}).chmod(0o755)
