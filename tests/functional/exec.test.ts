eca67d6c02919943e45893c0640e4dbb256847c4
Max Howell <mxcl@me.com>
2023-07-10 15:14:54 -0400

    Restore and fix these tests

--- tests/functional/exec.test.ts
+++ tests/functional/exec.test.ts
@@ -3,59 +3,77 @@ import { spy, stub, returnsNext } from "deno/testing/mock.ts"
 import { createTestHarness, newMockProcess } from "./testUtils.ts"
 import { TeaError } from "tea"
 
-// Deno.test("exec", { sanitizeResources: false, sanitizeOps: false }, async () => {
-//   const { run } = await createTestHarness(undefined)
-//   const { runspy } = await run(["node", "--version"])
-//   const foo = runspy.calls[0].args[0].cmd
-//   assertEquals(foo, ["node", "--version"], "should have run node --version")
-// })
+Deno.test("exec", { sanitizeResources: false, sanitizeOps: false }, async () => {
+  const { run, useRunInternals } = await createTestHarness()
 
-// Deno.test("forward env to exec", { sanitizeResources: false, sanitizeOps: false }, async () => {
-//   const { run, TEA_PREFIX } = await createTestHarness()
-//   const { runspy } = await run(["sh", "-c", "echo $TEA_PREFIX"])
-//   assertEquals(runspy.calls[0].args[0].env?.["TEA_PREFIX"], TEA_PREFIX.string)
-// })
+  const useRunSpy = spy(useRunInternals, "nativeRun")
+  try {
+    await run(["node", "--version"])
+  } finally {
+    useRunSpy.restore()
+  }
 
-// Deno.test("exec run errors", { sanitizeResources: false, sanitizeOps: false }, async test => {
-//   const tests = [
-//     {
-//       name: "normal error",
-//       procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new Error("test error")),
-//       expectedErr: "exiting with code: 1",
-//     },
-//     {
-//       name: "tea error",
-//       procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new TeaError("confused: interpreter", {})),
-//       expectedErr: "exiting with code: 1",
-//     },
-//     {
-//       name: "not found",
-//       procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new Deno.errors.NotFound()),
-//       expectedErr: "exiting with code: 127",
-//     },
-//     {
-//       name: "permission denied",
-//       procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new Deno.errors.PermissionDenied()),
-//       expectedErr: "exiting with code: 127",
-//     },
-//   ]
+  assertEquals(useRunSpy.calls[0].args[0].cmd, ["node", "--version"], "should have run node --version")
+})
 
-//   for (const { name, procStatus, expectedErr } of tests) {
-//     await test.step(name, async () => {
-//       const { run, useRunInternals } = await createTestHarness(undefined, false)
-//       const mockProc = newMockProcess(procStatus)
+Deno.test("forward env to exec", { sanitizeResources: false, sanitizeOps: false }, async () => {
+  const {run, TEA_PREFIX, useRunInternals } = await createTestHarness()
 
-//       const useRunStub = stub(useRunInternals, "nativeRun", returnsNext([mockProc]))
-//       await assertRejects(async () => {
-//         try {
-//           await run(["node", "--version"])
-//         } finally {
-//           useRunStub.restore()
-//         }
-//       }, expectedErr)
-//     })
-//   }
-// })
+  const useRunSpy = spy(useRunInternals, "nativeRun")
+  try {
+    await run(["sh", "-c", "echo $TEA_PREFIX"])
+  } finally {
+    useRunSpy.restore()
+  }
+
+  assertEquals(useRunSpy.calls[0].args[0].env?.["TEA_PREFIX"], TEA_PREFIX.string)
+})
+
+Deno.test("exec run errors", { sanitizeResources: false, sanitizeOps: false }, async test => {
+  const tests = [
+    {
+      name: "exit error",
+      procStatus: (): Promise<Deno.CommandStatus> => Promise.resolve({success: false, code: 123, signal: null}),
+      expectedErr: "exiting with code: 123",
+    },
+    {
+      name: "normal error",
+      procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new Error("test error")),
+      expectedErr: "exiting with code: 1",
+    },
+    {
+      name: "tea error",
+      procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new TeaError("confused: interpreter", {})),
+      expectedErr: "exiting with code: 1",
+    },
+    {
+      name: "not found",
+      procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new Deno.errors.NotFound()),
+      expectedErr: "exiting with code: 127",
+    },
+    {
+      name: "permission denied",
+      procStatus: (): Promise<Deno.CommandStatus> => Promise.reject(new Deno.errors.PermissionDenied()),
+      expectedErr: "exiting with code: 127",
+    },
+  ]
+
+  for (const { name, procStatus, expectedErr } of tests) {
+    await test.step(name, async () => {
+      const {run, useRunInternals } = await createTestHarness()
+      const mockProc = newMockProcess(procStatus)
+
+      const useRunStub = stub(useRunInternals, "nativeRun", returnsNext([mockProc]))
+      await assertRejects(async () => {
+        try {
+          await run(["node", "--version"])
+        } finally {
+          useRunStub.restore()
+        }
+      }, expectedErr)
+    })
+  }
+})
 
 Deno.test("exec forkbomb protector", { sanitizeResources: false, sanitizeOps: false }, async () => {
   const { run } = await createTestHarness()
