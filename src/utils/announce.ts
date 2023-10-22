53fe0b91d25d76f080ab8a36e103d12735b50b42
Max Howell <mxcl@me.com>
2023-10-22 08:02:03 -0400

    Fix colorization in shellcode errors
    This because deno’s console %c colorizer always removes color codes if stdout is not a tty. This doesn't apply to us in these cases because we are outputting to stderr and that *is* a tty.
    
    This is what happens when you are using a product that is mainly targeted at web-devs and not cli-devs.

--- src/utils/announce.ts
+++ src/utils/announce.ts
@@ -1,18 +1,18 @@
-import { dim } from './color.ts'
+import { dim, blurple } from './color.ts'
 
-export default function({ title, subtitle, body, help, color }: {title: string, subtitle?: string | undefined, body: (string[])[], help?: string | undefined, color?: string}) {
-  color = `color: ${color ?? '#5f5fff'}`
+export default function({ title, subtitle, body, help, color }: {title: string, subtitle?: string | undefined, body: (string[])[], help?: string | undefined, color?: (x: string) => string}) {
+  color ??= blurple
   help ??= 'unknown-error'
 
   if (subtitle) {
-    console.error('%c× %s %c%s', color, title, 'color: initial', subtitle)
+    console.error(`${color(`× %s`)} %s`, title, subtitle)
   } else {
-    console.error('%c× %s', color, title)
+    console.error(color('× %s'), title)
   }
   for (const [s1, ...ss] of body) {
-    console.error(`%c│%c ${s1 ?? ''}`, color, 'color: initial', ...ss)
+    console.error(`${color('│')} ${s1 ?? ''}`, ...ss)
   }
 
   const url = help.startsWith('http') ? help : `https://docs.pkgx.sh/help/${help}`
-  console.error('%c╰─➤%c %s', color, 'color: initial', dim(url))
+  console.error(`${color('╰─➤')} %s`, dim(url))
 }
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,5 +1,5 @@
-import { dim, rgb8, bgRgb8 } from "deno/fmt/colors.ts"
+import { dim, rgb8, bgRgb8, red } from "deno/fmt/colors.ts"
 
 export const blurple = (x: string) => rgb8(x, 63)
-export { dim }
+export { dim, red }
 export const inverse_blurple = (x: string) => bgRgb8(x, 63)
