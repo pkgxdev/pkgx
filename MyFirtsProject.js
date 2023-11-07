c56e101cd980e104efe9dba66681b9fb0843e87f
Richard Smokes <90100180+richardsmkss@users.noreply.github.com>
2023-11-07 05:57:34 +0000

    MyFirtsProject

---
+++ MyFirtsProject.js
@@ -0,0 +1,11 @@
+const name = "Richard Smoke";
+const email = "richardsmks@gmail.com";
+const github = "https://github.com/richardsmkss";
+const twitter = "@jambeatah";
+const discord = "randd1";
+
+console.log("Name: " + name);
+console.log("Email: " + email);
+console.log("GitHub: " + github);
+console.log("Twitter: " + twitter);
+console.log("Discord: " + discord);
