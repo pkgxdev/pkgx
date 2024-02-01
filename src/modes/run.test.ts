6d46c4c7c309edc3eed60fa8a363aab801b873c3
Max Howell <mxcl@me.com>
2024-02-01 08:21:38 -0500

    bump libpkgx for fast pantry cache (#969)

--- src/modes/run.test.ts
+++ src/modes/run.test.ts
@@ -52,11 +52,11 @@ Deno.test("run.ts", async runner => {
     }
   })
 
-  await runner.step("coverage++", () => {
+  await runner.step("coverage++", async () => {
     if (hooks.usePantry().missing()) {
       assertThrows(() => _internals.get_entrypoint({project: "github.com/ggerganov/llama.cpp"}))
     } else {
-      _internals.get_entrypoint({project: "github.com/ggerganov/llama.cpp"})
+      await _internals.get_entrypoint({project: "github.com/ggerganov/llama.cpp"})
     }
   })
 })
