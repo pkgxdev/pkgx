bdfc7cd4b40c2725cc470aa3e55650f3b1a8553c
Max Howell <mxcl@me.com>
2024-09-05 14:52:04 -0400

    sync log messages

--- src/prefab/failsafe.ts
+++ src/prefab/failsafe.ts
@@ -1,4 +1,5 @@
 import { PackageNotFoundError, hooks } from "pkgx"
+import Logger from "../utils/Logger.ts"
 const { useSync } = hooks
 
 export default async function failsafe<T>(body: () => Promise<T>): Promise<T> {
@@ -6,7 +7,19 @@ export default async function failsafe<T>(body: () => Promise<T>): Promise<T> {
     return await body()
   } catch (err) {
     if (err instanceof PackageNotFoundError) {
-      await _internals.useSync()
+      const logger: Logger = new Logger('sync')
+      const printer = {
+        syncing() {
+          logger.replace('fetching')
+        },
+        caching() {
+          logger.replace('caching')
+        },
+        syncd() {
+          logger.clear()
+        }
+      }
+      await _internals.useSync(printer)
       return body()
     } else {
       throw err
