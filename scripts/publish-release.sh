955cd31799300e6d5c016624f3eb794c370f3253
Max Howell <mxcl@me.com>
2025-01-17 10:08:12 -0500

    Don’t do some CD for 1.x releases

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -1,7 +1,4 @@
-#!/usr/bin/env -S pkgx +git +gh +jq +gum +npx +deno bash -eo pipefail
-
-# the back and forth here is because draft releases cannot trigger
-# GitHub Actions 😒
+#!/usr/bin/env -S pkgx +git +gh +jq +gum bash -eo pipefail
 
 if ! git diff-index --quiet HEAD --; then
   echo "error: dirty working tree" >&2
@@ -13,39 +10,10 @@ if [ "$(git rev-parse --abbrev-ref HEAD)" != main ]; then
   exit 1
 fi
 
-# ensure we have the latest version tags
-git fetch origin -pft
+v_new=$(cargo metadata --format-version 1 --no-deps | jq -r '.packages[] | select(.name == "pkgx") | .version')
 
-versions="$(git tag | grep '^v[0-9]\+\.[0-9]\+\.[0-9]\+')"
-v_latest="$(npx -- semver --include-prerelease $versions | tail -n1)"
-
-_is_prerelease() {
-  deno run - <<EoTS
-    import { parse } from "https://deno.land/x/semver/mod.ts";
-    console.log(parse('$1')?.prerelease.length ? 'true' : 'false');
-EoTS
-}
-
-case $1 in
-major|minor|patch|prerelease)
-  v_new=$(npx -- semver bump $v_latest --increment $1)
-  ;;
-"")
-  echo "usage $0 <major|minor|patch|prerelease|VERSION>" >&2
-  exit 1;;
-*)
-  v_new=$1
-  ;;
-esac
-
-if [ $v_new = $v_latest ]; then
-  echo "$v_new already exists!" >&2
-  exit 1
-fi
-
-is_prerelease=$(_is_prerelease $v_new)
-
-if ! gh release view v$v_new >/dev/null 2>&1; then
+case $(gh release view --json isDraft | jq .isDraft) in
+'release not found')
   gum confirm "prepare draft release for $v_new?" || exit 1
 
   gh release create \
@@ -56,13 +24,17 @@ if ! gh release view v$v_new >/dev/null 2>&1; then
     --notes-start-tag=v$v_latest \
     --title=v$v_new
 
-  git push origin main
-else
+  ;;
+true)
   gum format "> existing $v_new draft found, using that"
   echo  #spacer
+  ;;
+false)
+  gum format "$v_new already published! edit \`./crates/cli/Cargo.toml\`"
+  exit 1;;
+esac
 
-  git push origin main
-fi
+git push origin main
 
 gh workflow run cd.yml --raw-field version="$v_new"
 # ^^ infuriatingly does not tell us the ID of the run
