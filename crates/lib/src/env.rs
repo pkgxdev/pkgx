ae681156b5bef240cc4b85cf95ce0300553e27a6
Jacob Heider <jacob@pkgx.dev>
2025-01-17 15:55:29 -0500

    this fixes subprogs missing keys
    closes #1085

--- crates/lib/src/env.rs
+++ crates/lib/src/env.rs
@@ -122,11 +122,19 @@ fn suffixes(key: &EnvKey) -> Option<Vec<&'static str>> {
 pub fn mix(input: HashMap<String, Vec<String>>) -> HashMap<String, String> {
     let mut rv = HashMap::new();
 
+    // append the existing keys
     for (key, mut value) in std::env::vars() {
         if let Some(injected_values) = input.get(&key) {
             value = format!("{}:{}", injected_values.join(":"), value);
         }
         rv.insert(key, value);
+    }
+
+    // add the new keys
+    for (key, value) in input.iter() {
+        if !rv.contains_key(key) {
+            rv.insert(key.clone(), value.join(":"));
+        }
     }
 
     rv
