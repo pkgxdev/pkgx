5aad810ac779b45d08dffbe5141fdcb58a1d7aa7
Max Howell <mxcl@me.com>
2025-03-11 17:18:13 -0400

    Fix cross compiling for Windows

--- crates/lib/build.rs
+++ crates/lib/build.rs
@@ -1,8 +1,14 @@
+use std::env;
+
 fn main() {
-    #[cfg(unix)]
-    let dist_url = option_env!("PKGX_DIST_URL").unwrap_or("https://dist.pkgx.dev");
-    #[cfg(windows)]
-    let dist_url = option_env!("PKGX_DIST_URL").unwrap_or("https://dist.pkgx.dev/v2");
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    let dist_url = if target_os == "windows" {
+        option_env!("PKGX_DIST_URL").unwrap_or("https://dist.pkgx.dev/v2")
+    } else {
+        option_env!("PKGX_DIST_URL").unwrap_or("https://dist.pkgx.dev")
+    };
+
     let default_pantry_tarball_filename = "pantry.tgz";
     let pantry_url =
         option_env!("PKGX_PANTRY_TARBALL_FILENAME").unwrap_or(default_pantry_tarball_filename);
