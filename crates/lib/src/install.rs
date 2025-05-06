e238cc3972fc1f80327c64dab31f285899585c87
Max Howell <mxcl@me.com>
2025-05-06 12:51:24 -0400

    Fix --version outputting to stderr

--- crates/lib/src/install.rs
+++ crates/lib/src/install.rs
@@ -161,13 +161,22 @@ async fn symlink(installation: &Installation, config: &Config) -> Result<(), Box
     let minor_range = if installation.pkg.version.major > 0 {
         VersionReq::caret(&v_mm)?
     } else {
-        VersionReq::parse(&format!(">={},<{}", v_mm, format!("0.{}", installation.pkg.version.minor + 1)))?
+        VersionReq::parse(&format!(
+            ">={},<0.{}",
+            v_mm,
+            installation.pkg.version.minor + 1
+        ))?
     };
     let most_minor = versions
         .iter()
         .filter(|(version, _)| minor_range.satisfies(version))
         .next_back()
-        .ok_or_else(|| anyhow::anyhow!("Could not find most minor version for {}", installation.pkg.project))?;
+        .ok_or_else(|| {
+            anyhow::anyhow!(
+                "Could not find most minor version for {}",
+                installation.pkg.project
+            )
+        })?;
 
     if most_minor.0 != installation.pkg.version {
         return Ok(());
